cde3e7b290c4bcdddab3e3bde09441d2eba55a44
Elena Görgen <elenagorgen@elenas-mbp.local>
2018-06-16 13:04:00 +0200

    Initial Commit

---
+++ WeinbergApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeinbergApp
+//
+//  Created by Elena Görgen on 16.06.18.
+//  Copyright © 2018 Christoph Tölkes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
